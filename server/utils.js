3b85fe48fc6ac4f307bd8c9564a314f087666b67
ammarm08 <ammar.mian08@gmail.com>
2015-12-29 18:13:58 -0500

    text back user whether his records were found

--- server/utils.js
+++ server/utils.js
@@ -21,19 +21,22 @@ exports.handleTextMessage = function(twilioBody, twilioClient, twilioNum, callba
   // check if user exists, if not, text back telling user to sign up on website first.
   User.findOne({phone_number: twilioBody.From}, function(err, user) {
     if (!user || err) {
-      var message = "No user found. Create an account at regulate.herokuapp.com!";
-      sendMessage(twilioClient, twilioBody.From, twilioNum, message);
-      callback("No user found.", null);
+      var notFound = "No user found. Create an account at regulate.herokuapp.com!";
+      sendMessage(twilioClient, twilioBody.From, twilioNum, notFound);
+      callback(notFound, null);
     } else {
-      parseText(user, twilioBody.Body, function(err, message) {
-        if (err) {
-          sendMessage(twilioClient, twilioBody.From, twilioNum, err);
-          return callback(err, null);
-        } else {
-          sendMessage(twilioClient, twilioBody.From, twilioNum, message);
-          return callback(null, message);
-        }
-      }
+      var found = "Found!";
+      sendMessage(twilioClient, twilioBody.From, twilioNum, found);
+      callback(null, found);
+      // parseText(user, twilioBody.Body, function(err, message) {
+      //   if (err) {
+      //     sendMessage(twilioClient, twilioBody.From, twilioNum, err);
+      //     return callback(err, null);
+      //   } else {
+      //     sendMessage(twilioClient, twilioBody.From, twilioNum, message);
+      //     return callback(null, message);
+      //   }
+      // }
     }
   });
 
@@ -64,45 +67,45 @@ var findUser = function (options, callback) {
   })
 };
 
-var parseText = function (user, messageBody, callback) {
+// var parseText = function (user, messageBody, callback) {
 
-  if (messageBody.length < 3) return callback("Invalid message length", null);
+//   if (messageBody.length < 3) return callback("Invalid message length", null);
 
-  // clean up response text
-  var messages = messageBody.split(",");
-  var booleanVal, bool;
+//   // clean up response text
+//   var messages = messageBody.split(",");
+//   var booleanVal, bool;
 
-  // error handling: not long enough + first arg is not a number
-  if (messages.length < 3) return callback("Invalid format: Not enough arguments", null);
-  if (isNaN(parseInt(messages[0]))) return callback("Invalid format: First argument must be a number", null);
+//   // error handling: not long enough + first arg is not a number
+//   if (messages.length < 3) return callback("Invalid format: Not enough arguments", null);
+//   if (isNaN(parseInt(messages[0]))) return callback("Invalid format: First argument must be a number", null);
 
-  messages = [messages[0], messages[1], messages.slice(2, messages.length).join()];
+//   messages = [messages[0], messages[1], messages.slice(2, messages.length).join()];
 
-  for (var i = 0; i < messages.length; i++) {
-    var word = messages[i];
-    messages[i] = word.replace(/\s*/, "");
-  }
+//   for (var i = 0; i < messages.length; i++) {
+//     var word = messages[i];
+//     messages[i] = word.replace(/\s*/, "");
+//   }
 
-  // error handling: second argument isn't a yes/no.
-  booleanVal = messages[1].toLowerCase(); 
-  if (booleanVal !== 'yes' || booleanVal !== 'no') return callback("Invalid format: Second arg must be yes or no", null);
-  bool = booleanVal === yes ? true : false;
+//   // error handling: second argument isn't a yes/no.
+//   booleanVal = messages[1].toLowerCase(); 
+//   if (booleanVal !== 'yes' || booleanVal !== 'no') return callback("Invalid format: Second arg must be yes or no", null);
+//   bool = booleanVal === yes ? true : false;
 
-  //Write to DB
-  var newData = new Data({
-    user: user._id,
-    emotion: parseInt(message[0]),
-    hydrate: bool,
-    note: message[2],
-    date: new Date()
-  });
+//   //Write to DB
+//   var newData = new Data({
+//     user: user._id,
+//     emotion: parseInt(message[0]),
+//     hydrate: bool,
+//     note: message[2],
+//     date: new Date()
+//   });
 
-  newData.save(function(err, data) {
-    if (err) return callback("Error saving your response to our database. Try in a few hours", null);
-    callback(null, "Successfully recorded your response!");
-  })
+//   newData.save(function(err, data) {
+//     if (err) return callback("Error saving your response to our database. Try in a few hours", null);
+//     callback(null, "Successfully recorded your response!");
+//   })
 
-};
+// };
 
 var sendMessage = function (client, recipientNum, twilioNum, response) {
   client.messages.create({ 
