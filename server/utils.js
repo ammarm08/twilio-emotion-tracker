b09b106c31b63e8334920447b989e816f06beaf6
ammarm08 <ammar.mian08@gmail.com>
2015-12-29 22:35:46 -0500

    send message before writing data

--- server/utils.js
+++ server/utils.js
@@ -29,14 +29,13 @@ exports.handleTextMessage = function(twilioBody, twilioClient, twilioNum, callba
       callback(notFound, null);
     } else {
       var parsed = parseMessage(twilioBody.Body);
-      sendMessage(twilioClient, twilioBody.From, twilioNum, JSON.stringify(twilioBody));
-      callback(null, parsed);
-      // writeData(user, parsed, function(err, data) {
-      //   if (err) return callback(err, null);
-      //   console.log(data);
-      //   sendMessage(twilioClient, twilioBody.From, twilioNum, "Got it.");
-      //   callback(null, data);
-      // });
+      sendMessage(twilioClient, twilioBody.From, twilioNum, "Got it.");
+      writeData(user, parsed, function(err, data) {
+        if (err) return callback(err, null);
+        console.log(data);
+        sendMessage(twilioClient, twilioBody.From, twilioNum, "Got it.");
+        callback(null, data);
+      });
     }
   });
 
