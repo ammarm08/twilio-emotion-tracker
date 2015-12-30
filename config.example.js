0719c99c86c9a12e25d5d87324392d13db576688
ammarm08 <ammar.mian08@gmail.com>
2015-12-30 01:50:27 -0500

    example config file

---
+++ config.example.js
@@ -0,0 +1,19 @@
+/*
+STEPS:
+
+1. Add your keys
+2. Rename file to config.js
+
+*/
+
+module.exports = {
+  google: {
+    clientID: "YOUR_GOOGLE_API_CLIENT_ID",
+    clientSecret: "YOUR_GOOGLE_API_CLIENT_SECRET"
+  },
+  twilio: {
+    account: "YOUR_TWILIO_ACCOUNT_SID",
+    token: "YOUR_TWILIO_AUTH_TOKEN",
+    number: "YOUR_TWILIO_PHONE_NUMBER"
+  }
+};
