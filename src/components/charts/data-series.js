aebb23784aab18a9d839af8c49b397393c47749f
ammarm08 <ammar.mian08@gmail.com>
2016-01-08 16:19:52 -0500

    responsive bar chart width in tooltip hover

--- src/components/charts/data-series.js
+++ src/components/charts/data-series.js
@@ -10,36 +10,21 @@ var DataSeries = React.createClass({
   },
 
   setCircleHover: function(data) {
-    var readDate = d3.time.format("%B %d, %Y");
+    
     var tooltip = d3.select(".tooltip");
 
-    var renderTooltipChart = function(point) {
-      var width = (+point/10*100).toString() + "%";
-      var chart = '<div class="tooltip-chart" max-width=' + width + '>' + point + '</div>';
-      return '<div class="tooltip-chart-container">' + chart + '</div>';
-    };
-
-    var tooltipTemplate = function(point) {
-
-      var date = '<thead><tr><th>' + readDate(point.date) + '</th></tr></thead>';
-      var hr = '<tr><td><hr/></td></tr>';
-      var emotion = '<tr><td>' + renderTooltipChart(point.emotion) + '</td></tr>';
-      var hydration = '<tr><td>' + point.hydrate + '</td></tr>';
-      var note = '<tr><td>' + point.note + '</td></tr>';
-
-      return '<table><tbody>' + date + hr + emotion + hydration + note + '</tbody></table>';
-    };
-
     d3.selectAll("circle").data(data)
-                          .on("mouseover", function(d) {
-                            tooltip.transition().duration(200).style("opacity", .9);
-                            tooltip.html(tooltipTemplate(d))
-                                   .style("left", (d3.event.pageX + 20) + "px")
-                                   .style("top", (d3.event.pageY - 30) + "px");
-                          })
-                          .on("mouseout", function(d) {
-                            tooltip.transition().duration(500).style("opacity", 0);
-                          });
+      .on("mouseover", function(d) {
+        tooltip.transition().duration(200).style("opacity", .9);
+        tooltip.html(helpers.templateTooltip(d))
+               .style("left", (d3.event.pageX + 20) + "px")
+               .style("top", (d3.event.pageY - 30) + "px");
+        var width = (d.emotion*10).toString();
+        d3.select(".tooltip-chart").style("width", width + "%");
+      })
+      .on("mouseout", function(d) {
+        tooltip.transition().duration(500).style("opacity", 0);
+      })
   },
 
   render: function() {
@@ -62,6 +47,29 @@ var DataSeries = React.createClass({
     );
   }
 });
+
+var helpers = {
+
+  templateTooltip: function(point) {
+    var readDate = d3.time.format("%B %d, %Y");
+
+    var date = '<thead><tr><th>' + readDate(point.date) + '</th></tr></thead>';
+    var hr = '<tr><td><hr/></td></tr>';
+    var emotion = '<tr><td>' + helpers.renderTooltipChart(point.emotion) + '</td></tr>';
+    var hydration = '<tr><td>' + point.hydrate + '</td></tr>';
+    var note = '<tr><td>' + point.note + '</td></tr>';
+
+    return '<table><tbody>' + date + hr + emotion + hydration + note + '</tbody></table>';
+  },
+
+  renderTooltipChart: function(point) {
+    var data = '<span class="data-emotion">' + point + '</span>';
+    var chart = '<div class="tooltip-chart">' + data + '</div>';
+    return '<div class="tooltip-chart-container">' + chart + '</div>';
+  }
+
+};
+
 
 
 module.exports = DataSeries;
