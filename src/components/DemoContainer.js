d054fa305e159723d2ac62afd8bcbd8e9337b4da
ammarm08 <ammar.mian08@gmail.com>
2016-01-10 14:43:29 -0500

    demo page svg dimensions update

--- src/components/DemoContainer.js
+++ src/components/DemoContainer.js
@@ -37,8 +37,8 @@ var DemoContainer = React.createClass({
   render: function() {
 
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = 800 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom,
+        width = 960 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom,
         x = d3.time.scale.utc().range([0, width]),
         y = d3.scale.linear().range([height,0]),
         xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(10),
